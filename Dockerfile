7bfa4138ae291997be1e88174bee572c75c58d7a
shunsuke-kawata <kwt20238120@gmail.com>
2024-02-11 05:05:45 +0900

    update youtubeのapiキーを作成

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ RUN apt-get update
 RUN apt-get -y install locales && \
     localedef -f UTF-8 -i ja_JP ja_JP.UTF-8
 RUN apt -y update && apt -y upgrade
-RUN apt -y install libopencv-dev
 ENV LANG ja_JP.UTF-8
 ENV LANGUAGE ja_JP:ja
 ENV LC_ALL ja_JP.UTF-8
--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import os
 from fastapi import FastAPI
 import uvicorn
+from dotenv import load_dotenv
 
 app = FastAPI()
 
+load_dotenv()
 
+api = os.environ['YOUTUBE_DATA_API_KEY']
 @app.get("/")
 def read_root():
-    return {"app": "jarujaru"}
+    return {"api": api }
 
 if __name__ == "__main__":
     uvicorn.run(app,port=8000)
