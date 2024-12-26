4165915b40e84df9c576860f7473106f5f5e6ccb
shunsuke-kawata <kwt20238120@gmail.com>
2024-12-27 01:33:33 +0900

    refactor 関数の分離 && update 動画の処理を効率化

--- router/question_router.py
+++ router/question_router.py
@@ -1,221 +1,96 @@
-import os
-import sys
-sys.path.append('../')
-import config
-from fastapi import APIRouter, HTTPException,status,Query
-from fastapi.responses import JSONResponse,Response
-import requests
-from typing import List
-from yt_dlp import YoutubeDL
-import random
 import io
+import os
+import aiofiles
+from fastapi import APIRouter, HTTPException, status, Query
+from fastapi.responses import JSONResponse, StreamingResponse
+import config
 from pydub import AudioSegment
+import random
+import tempfile
+from typing import List
+from utils.util import get_answer_title, get_infomation_of_playlist, download_mp3
 
-def get_infomation_of_playlist(playlist_url:str):
-    # オプションを指定
-    ydl_opts = {
-        'quiet': True,
-        'extract_flat': True,
-    }
-    #実際のダウンロードは行わない
-    with YoutubeDL(ydl_opts) as ydl:
-        # プレイリストから情報を取得
-        playlist_info = ydl.extract_info(playlist_url, download=False)
-        return playlist_info
-
-def get_answer_title(original_title:str,group:int):
-    return_title = ""
-    if(group==1):
-        start_i = original_title.find('『')
-        end_i = original_title.find('』')
-        if(end_i==-1):
-            end_i = original_title.find('【')
-        return_title = original_title[start_i+1:end_i]
-    elif(group==2):
-        str_end = original_title[-1]
-        if(str_end=='】'):
-            start_i = original_title.find('】',4)
-            end_i = original_title.find('【',4)
-        elif(str_end=='』'):
-            start_i = original_title.find('『')
-            end_i = original_title.find('』')
-        else:
-            start_i = original_title.find('】',4)
-            end_i = len(original_title)
-        return_title = original_title[start_i+1:end_i]
-    elif(group==3):
-        start_i = original_title.find('『')
-        end_i = original_title.find('』')
-        if(start_i==-1 and end_i==-1):
-            start_i = original_title.find('】',4)
-            end_i = len(original_title)
-        return_title = original_title[start_i+1:end_i]
-    elif(group==4):
-        start_i = original_title.find('『')
-        end_i = original_title.find('』')
-        brackets_flag = original_title.find('【',4)
-        
-        if(end_i==-1 and brackets_flag==-1):
-            return_title = original_title
-        elif(end_i==-1 and brackets_flag!=-1):
-            return_title = original_title[:brackets_flag]
-        else:
-            return_title = original_title[start_i+1:end_i]
-    elif(group==5):
-        pass
-    return return_title
-
-    
-def download_mp3(playlist_url,playlist_items,max_downloads=1):
-    #オプションを指定
-    ydl_opts = {
-            'outtmpl' : './origin_mp3/%(id)s.%(ext)s',
-            'format' : 'bestaudio',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',  
-                'preferredcodec': 'mp3',  #変換したい形式を指定
-                'preferredquality': '192' #ビットレートを指定
-            }],
-            'playlist_items': str(playlist_items)
-    }
-    with YoutubeDL(ydl_opts) as ydl:
-        playlist_info = ydl.extract_info(playlist_url, download=True)
-        return playlist_info
-
-#分割したエンドポイントの作成
+# エンドポイントの作成
 question_endpoint = APIRouter()
 
-@question_endpoint.get("/question/", tags=["question"])
-def root():
-    return {"root": "question-endpoint"}
-
-#まとめて問題を作成する
-@question_endpoint.get("/question/questions", tags=["question"])
-def questions(playlist_id: List[str] = Query(..., title="Playlist IDs", description="IDs of the playlists to retrieve"),
-              num:int=Query(...,title="Number", description="Number Of Questions")):
-    if playlist_id is None or num is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Both 'num' and 'playlist_id' parameters are required.")
-
-    id_list = []
-    print('start')
-    for _ in range(num):
-        selected_playlist_id = random.choice(playlist_id)
-        playlist_url = f"https://www.youtube.com/playlist?list={selected_playlist_id}"
-        try:
-            download_mp3(playlist_url=playlist_url,max_downloads=1)
-        except Exception as e:
-            print(e)
-            
-    
-    if(len(id_list)==0):
-        error_message = {
-            "message": "Internal Server Error",
-            "domain": "global",
-            "reason": "An error occurred while creating the question"
-        }
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=error_message)
-
-    return JSONResponse(status_code=status.HTTP_200_OK, content=id_list)
-
-
-#単体の問題を作る
 @question_endpoint.get("/question/download", tags=["question"])
-def download(playlist_id: List[str] = Query(..., title="Playlist IDs", description="IDs of the playlists to retrieve")):
-    if playlist_id is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Both 'num' and 'playlist_id' parameters are required.")
+async def download(playlist_id: List[str] = Query(..., title="Playlist IDs")):
+    if not playlist_id:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Playlist IDs are required.")
 
-        
     selected_playlist_id = random.choice(playlist_id)
     playlist_url = f"https://www.youtube.com/playlist?list={selected_playlist_id}"
-    #プレイリストの情報を取得
+
     try:
-        info=get_infomation_of_playlist(playlist_url=playlist_url)
+        info = get_infomation_of_playlist(playlist_url=playlist_url)
     except Exception as e:
-        print(e)
-        error_message = {
-            "message": "Internal Server Error",
-            "domain": "global",
-            "reason": "An error occurred while creating the question"
-        }
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=error_message)
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={
+            "message": "Failed to fetch playlist information",
+            "reason": str(e)
+        })
 
     playlist_count = info.get('playlist_count')
-    
-    if(playlist_count is None):
-        error_message = {
-            "message": "Internal Server Error",
-            "domain": "global",
-            "reason": "An error occurred while creating the question"
-        }
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=error_message)
-       
-        
-    selected_index = random.randint(1,playlist_count)
-    result_info = download_mp3(playlist_url=playlist_url,playlist_items=selected_index)
-    
-    if(len(result_info.get('entries'))>0):
-        video_info = result_info.get('entries')[0]
-        id = video_info.get('id')
-        title = video_info.get('title')
-        group = None
+    if not playlist_count:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Invalid playlist data")
+
+    selected_index = random.randint(1, playlist_count)
+    result_info = download_mp3(playlist_url=playlist_url, playlist_items=selected_index)
+
+    if len(result_info.get('entries', [])) > 0:
+        video_info = result_info['entries'][0]
+        video_id = video_info['id']
+        title = video_info['title']
+        print(selected_playlist_id)
         if(config.JARUJARU_TOWER_PLAYLISTS.get(selected_playlist_id) is not None):
             group = config.JARUJARU_TOWER_PLAYLISTS.get(selected_playlist_id).get('group')
 
         elif(config.JARUJARU_ISLAND_PLAYLISTS.get(selected_playlist_id) is not None):
             group = config.JARUJARU_ISLAND_PLAYLISTS.get(selected_playlist_id)
-        if(group is None):
-            error_message = {
-                "message": "Internal Server Error",
-                "domain": "global",
-                "reason": "An error occurred while creating the question"
-            }
-            return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=error_message)
 
-        answer_title = get_answer_title(original_title=title,group=group)
+        if not group:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Group not found")
+
+        answer_title = get_answer_title(original_title=title, group=group)
         question_dict = {
-            'id':id,
-            'title':answer_title,
-            'original_file_path':f"./origin_mp3/{id}.mp3"
+            'id': video_id,
+            'title': answer_title,
+            'original_file_path': f"./origin_mp3/{video_id}.mp3"
         }
-        # ローカルのMP3ファイルをバイナリモードで読み込み
-            
+
         return JSONResponse(status_code=status.HTTP_200_OK, content=question_dict)
-    
-    error_message = {
-        "message": "Internal Server Error",
-        "domain": "global",
-        "reason": "An error occurred while creating the question"
-     }
-    return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=error_message)
 
-#idを指定してmp3ファイルを返却する
+    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="No entries found in the playlist")
+
+#ダウンロードしたデータをバイト形式で返却する
 @question_endpoint.get("/question/fetch/{video_id}", tags=["question"])
-def fetch(video_id):
+async def fetch(video_id: str):
     mp3_path = f"./origin_mp3/{video_id}.mp3"
-     # ファイルが存在するか確認します
-    if not os.path.exists(mp3_path):
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,content="File not found")
 
-    origimnal_mp3 = AudioSegment.from_file(file=mp3_path, format="mp3")
-    
-    if(origimnal_mp3.duration_seconds>180):
-        trimmed_mp3 = origimnal_mp3[:180*1000]
-    else:
-        trimmed_mp3 = origimnal_mp3
-    
+    if not os.path.exists(mp3_path):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="File not found")
+
+    # 一時ファイルを作成して処理
     try:
-        # trimmed_mp3.export(f"./trimmed_mp3/{video_id}.mp3", format="mp3")
-            # BytesIO を使用してファイルをメモリ上で処理
-        return_buttfer = io.BytesIO()
-        trimmed_mp3.export(return_buttfer, format="mp3")
-        return_buttfer.seek(0)
-        os.remove(mp3_path)
-        return Response(content=return_buttfer.getvalue(), media_type="audio/mp3")
-        
-    except:
-        error_message = {
-            "message": "Internal Server Error",
-            "domain": "global",
-            "reason": "An error occurred while creating the question"
-        }
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=error_message)
+        async with aiofiles.open(mp3_path, mode="rb") as file:
+            file_data = await file.read()
+
+        audio = AudioSegment.from_file(io.BytesIO(file_data), format="mp3")
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_file:
+            audio.export(tmp_file.name, format="mp3")
+            tmp_file_path = tmp_file.name
+
+        # ファイルをストリームとして返す
+        async def file_streamer():
+            async with aiofiles.open(tmp_file_path, mode="rb") as tmp_file:
+                while chunk := await tmp_file.read(1024):
+                    yield chunk
+            os.remove(tmp_file_path)  # 一時ファイルの削除
+
+        return StreamingResponse(file_streamer(), media_type="audio/mp3")
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    finally:
+        if os.path.exists(mp3_path):
+            os.remove(mp3_path)  # 元のMP3ファイルを削除
---
+++ utils/util.py
@@ -0,0 +1,81 @@
+import re
+from yt_dlp import YoutubeDL,utils
+
+#動画音声を取得する関数
+def download_mp3(playlist_url:str, playlist_items:int, max_downloads=1):
+    # オプションを指定
+    ydl_opts = {
+        'outtmpl': './origin_mp3/%(id)s.%(ext)s',
+        'format': 'bestaudio',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',  
+            'preferredcodec': 'mp3',  # 変換したい形式を指定
+            'preferredquality': '192'  # ビットレートを指定
+        }],
+        'playlist_items': str(playlist_items),
+        'download_ranges': utils.download_range_func([], [[0.0, 120.0]]),  # 最初の2分間を指定
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        playlist_info = ydl.extract_info(playlist_url, download=True)
+        return playlist_info
+    
+#プレイリストの情報を返却する関数
+def get_infomation_of_playlist(playlist_url:str):
+    # オプションを指定
+    ydl_opts = {
+        'quiet': True,
+        'extract_flat': True,
+    }
+    #実際のダウンロードは行わない
+    with YoutubeDL(ydl_opts) as ydl:
+        # プレイリストから情報を取得
+        playlist_info = ydl.extract_info(playlist_url, download=False)
+        return playlist_info
+  
+  
+#youtube公式の動画タイトルからネタタイトルを抽出する関数  
+def get_answer_title(original_title:str,group:int):
+    return_title = ""
+    if(group==1):
+        match = re.search(r'[『【](.+?)[』】]', original_title)
+        if match:
+            return_title = match.group(1)
+    elif(group==2):
+        str_end = original_title[-1]
+        if str_end == '】':
+            # 開始記号が '】' で終わっている場合、4文字目以降の '】' と '【' の間を抽出
+            match = re.search(r'】(.*?)【', original_title[4:])
+        elif str_end == '』':
+            # '』' で終わっている場合、全体の '『' と '』' の間を抽出
+            match = re.search(r'『(.*?)』', original_title)
+        else:
+            # それ以外の場合、4文字目以降の '】' 以降のすべてを抽出
+            match = re.search(r'】(.*)$', original_title[4:])
+        
+        if match:
+           return_title = match.group(1)  # 抽出部分を返す
+    elif(group==3):
+        match = re.search(r'『(.*?)』', original_title)  # '『' と '』' の間を抽出
+        if not match:  # マッチしない場合
+            match = re.search(r'】(.*)$', original_title[4:])  # 4文字目以降の '】' 以降を抽出
+        
+        if match:
+            return_title = match.group(1)  # 抽出部分を返す
+    elif(group==4):
+    # 正規表現で『』の間を優先的に抽出
+        match = re.search(r'『(.*?)』', original_title)
+        
+        if match:
+            # 『』が見つかった場合、その中身を返す
+            return_title =  match.group(1)
+        
+        # 『』が見つからない場合、【】の位置を確認
+        match = re.search(r'(.*?)【', original_title[4:])
+        if match:
+            # 4文字目以降で【が見つかった場合、その前の部分を返す
+            return_title = original_title[:4] + match.group(1)
+        
+    elif(group==5):
+        pass
+    return return_title
